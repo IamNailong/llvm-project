2e62988ab293aa323aafafe18941d7d6bb608d78
iamnailong <zhoujingfightinghahaha@gmail.com>
2025-12-07 21:56:28 +0800

    [Triton 1/10] Recognize Triton in triple parsing code.

--- llvm/lib/TargetParser/Triple.cpp
+++ llvm/lib/TargetParser/Triple.cpp
@@ -85,6 +85,7 @@ StringRef Triple::getArchTypeName(ArchType Kind) {
   case x86_64:         return "x86_64";
   case xcore:          return "xcore";
   case xtensa:         return "xtensa";
+  case triton:         return "triton";
   }
 
   llvm_unreachable("Invalid ArchType!");
@@ -249,6 +250,8 @@ StringRef Triple::getArchTypePrefix(ArchType Kind) {
   case dxil:        return "dx";
 
   case xtensa:      return "xtensa";
+
+  case triton:       return "triton";
   }
 }
 
@@ -491,6 +494,7 @@ Triple::ArchType Triple::getArchTypeForLLVMName(StringRef Name) {
     .Case("loongarch64", loongarch64)
     .Case("dxil", dxil)
     .Case("xtensa", xtensa)
+    .Case("triton", triton)
     .Default(UnknownArch);
 }
 
@@ -637,6 +641,7 @@ static Triple::ArchType parseArch(StringRef ArchName) {
                  "dxilv1.4", "dxilv1.5", "dxilv1.6", "dxilv1.7", "dxilv1.8",
                  Triple::dxil)
           .Case("xtensa", Triple::xtensa)
+          .Case("triton", Triple::triton)
           .Default(Triple::UnknownArch);
 
   // Some architectures require special parsing logic just to compute the
@@ -980,6 +985,7 @@ static Triple::ObjectFormatType getDefaultFormat(const Triple &T) {
   case Triple::ve:
   case Triple::xcore:
   case Triple::xtensa:
+  case Triple::triton:
     return Triple::ELF;
 
   case Triple::mipsel:
@@ -1703,6 +1709,7 @@ unsigned Triple::getArchPointerBitWidth(llvm::Triple::ArchType Arch) {
   case llvm::Triple::x86:
   case llvm::Triple::xcore:
   case llvm::Triple::xtensa:
+  case llvm::Triple::triton:
     return 32;
 
   case llvm::Triple::aarch64:
@@ -1811,6 +1818,7 @@ Triple Triple::get32BitArchVariant() const {
   case Triple::x86:
   case Triple::xcore:
   case Triple::xtensa:
+  case Triple::triton:
     // Already 32-bit.
     break;
 
@@ -1862,6 +1870,7 @@ Triple Triple::get64BitArchVariant() const {
   case Triple::tcele:
   case Triple::xcore:
   case Triple::xtensa:
+  case Triple::triton:
     T.setArch(UnknownArch);
     break;
 
@@ -2070,6 +2079,7 @@ bool Triple::isLittleEndian() const {
   case Triple::x86_64:
   case Triple::xcore:
   case Triple::xtensa:
+  case Triple::triton:
     return true;
   default:
     return false;
